048fa608b35b2195db7bd4ef15568d0b28d7b175
PXgamer <owzie123@gmail.com>
2017-09-25 12:58:12 +0100

    Added PhpDoc comments for WorldWideTorrents

--- src/WorldWideTorrents.php
+++ src/WorldWideTorrents.php
@@ -2,10 +2,20 @@
 
 namespace pxgamer\TorrentParser;
 
+/**
+ * Class WorldWideTorrents
+ * @package pxgamer\TorrentParser
+ */
 class WorldWideTorrents
 {
     const BASE_URL = 'https://worldwidetorrents.me';
 
+    /**
+     * Search for a specific query string
+     *
+     * @param string $search_query
+     * @return mixed
+     */
     public static function search($search_query)
     {
         $search_query = urlencode($search_query);
@@ -13,11 +23,22 @@ class WorldWideTorrents
         return self::get('/json.php?dllink=1&q=' . $search_query);
     }
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/json.php');
     }
 
+    /**
+     * Search for torrents by a specific username
+     *
+     * @param string $username
+     * @return mixed
+     */
     public static function user($username)
     {
         $username = urlencode($username);
@@ -25,6 +46,12 @@ class WorldWideTorrents
         return self::get('/json.php?username=' . $username);
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/json.php')
     {
         $cu = curl_init();
