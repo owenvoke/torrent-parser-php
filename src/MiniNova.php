fed57169cf59e8cc40671d174b769838b2fe9ab1
PXgamer <owzie123@gmail.com>
2017-09-25 13:05:02 +0100

    Added PhpDoc comments for MiniNova

--- src/MiniNova.php
+++ src/MiniNova.php
@@ -4,12 +4,22 @@ namespace pxgamer\TorrentParser;
 
 use pxgamer\TorrentParser\Traits\Parser;
 
+/**
+ * Class MiniNova
+ * @package pxgamer\TorrentParser
+ */
 class MiniNova
 {
     use Parser;
 
     const BASE_URL = 'http://www.mininova.org';
 
+    /**
+     * Search for a specific query string
+     *
+     * @param string $search_query
+     * @return mixed
+     */
     public static function search($search_query)
     {
         $search_query = urlencode($search_query);
@@ -17,11 +27,22 @@ class MiniNova
         return self::get('/rss/'.$search_query);
     }
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/rss.xml');
     }
 
+    /**
+     * Search for torrents by a specific username
+     *
+     * @param string $username
+     * @return mixed
+     */
     public static function user($username)
     {
         $username = urlencode($username);
@@ -29,6 +50,12 @@ class MiniNova
         return self::get('/rss.xml?user='.$username);
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/rss.xml')
     {
         $cu = curl_init();
