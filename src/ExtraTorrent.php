594c0ff6245833eb2f0efcf8a51a3b216f25d984
PXgamer <owzie123@gmail.com>
2017-09-25 13:00:54 +0100

    Added PhpDoc comments for ExtraTorrent

--- src/ExtraTorrent.php
+++ src/ExtraTorrent.php
@@ -4,12 +4,22 @@ namespace pxgamer\TorrentParser;
 
 use pxgamer\TorrentParser\Traits\Parser;
 
+/**
+ * Class ExtraTorrent
+ * @package pxgamer\TorrentParser
+ */
 class ExtraTorrent
 {
     use Parser;
 
     const BASE_URL = 'https://extra.to';
 
+    /**
+     * Search for a specific query string
+     *
+     * @param string $search_query
+     * @return mixed
+     */
     public static function search($search_query)
     {
         $search_query = urlencode($search_query);
@@ -17,11 +27,22 @@ class ExtraTorrent
         return self::get('/rss.xml?type=search&search=' . $search_query);
     }
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/rss.xml');
     }
 
+    /**
+     * Search for torrents by a specific username
+     *
+     * @param string $username
+     * @return mixed
+     */
     public static function user($username)
     {
         $username = urlencode($username);
@@ -29,6 +50,12 @@ class ExtraTorrent
         return self::get('/rss.xml?type=user&user=' . $username);
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/rss.xml')
     {
         $cu = curl_init();
