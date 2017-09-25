7ffe7df557b3fa76c817c29f3391dc994a1d07ec
PXgamer <owzie123@gmail.com>
2017-09-25 14:04:18 +0100

    Fully deprecated and removed tests for KatCR

--- src/KatCR.php
+++ src/KatCR.php
@@ -7,6 +7,8 @@ use pxgamer\TorrentParser\Traits\Parser;
 /**
  * Class KatCR
  * @package pxgamer\TorrentParser
+ *
+ * @deprecated 2.1.0 KatCR no longer supports RSS feeds.
  */
 class KatCR
 {
@@ -17,35 +19,10 @@ class KatCR
     /**
      * Get the latest torrents
      *
-     * @return mixed
+     * @return null
      */
     public static function latest()
     {
-        return self::get('/new/rss.php?dllink=1');
-    }
-
-    /**
-     * Perform a GET request
-     *
-     * @param string $endpoint
-     * @return mixed
-     */
-    private static function get($endpoint = '/new/rss.php')
-    {
-        $cu = curl_init();
-        curl_setopt_array(
-            $cu,
-            [
-                CURLOPT_URL => self::BASE_URL.$endpoint,
-                CURLOPT_SSL_VERIFYPEER => 0,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_RETURNTRANSFER => 1,
-            ]
-        );
-        $response = curl_exec($cu);
-        $response = str_replace('&nbsp;', ' ', $response);
-        $xml = simplexml_load_string($response);
-
-        return self::xml2array($xml)['channel'][0]['item'];
+        return null;
     }
 }
