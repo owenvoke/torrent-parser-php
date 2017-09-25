f6bfe28b7b3a3b38a778c670238c8e54264b9661
PXgamer <owzie123@gmail.com>
2017-09-25 12:58:56 +0100

    Added PhpDoc comments for KatCR

--- src/KatCR.php
+++ src/KatCR.php
@@ -4,17 +4,32 @@ namespace pxgamer\TorrentParser;
 
 use pxgamer\TorrentParser\Traits\Parser;
 
+/**
+ * Class KatCR
+ * @package pxgamer\TorrentParser
+ */
 class KatCR
 {
     use Parser;
 
     const BASE_URL = 'https://katcr.co';
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/new/rss.php?dllink=1');
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/new/rss.php')
     {
         $cu = curl_init();
