322c3c88593500397ca8c27cb7a170fafcf548a7
PXgamer <owzie123@gmail.com>
2017-09-25 12:59:59 +0100

    Added PhpDoc comments for EZTV

--- src/EZTV.php
+++ src/EZTV.php
@@ -4,17 +4,32 @@ namespace pxgamer\TorrentParser;
 
 use pxgamer\TorrentParser\Traits\Parser;
 
+/**
+ * Class EZTV
+ * @package pxgamer\TorrentParser
+ */
 class EZTV
 {
     use Parser;
 
     const BASE_URL = 'https://eztv.ag';
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/ezrss.xml');
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/ezrss.xml')
     {
         $cu = curl_init();
