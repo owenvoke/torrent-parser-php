e9d2d5d778ebfa779686e5c5bc320453bacd6803
PXgamer <owzie123@gmail.com>
2017-09-25 12:59:33 +0100

    Added PhpDoc comments for RARBG

--- src/RARBG.php
+++ src/RARBG.php
@@ -4,17 +4,32 @@ namespace pxgamer\TorrentParser;
 
 use pxgamer\TorrentParser\Traits\Parser;
 
+/**
+ * Class RARBG
+ * @package pxgamer\TorrentParser
+ */
 class RARBG
 {
     use Parser;
 
     const BASE_URL = 'https://rarbg.to';
 
+    /**
+     * Get the latest torrents
+     *
+     * @return mixed
+     */
     public static function latest()
     {
         return self::get('/rssdd_magnet.php');
     }
 
+    /**
+     * Perform a GET request
+     *
+     * @param string $endpoint
+     * @return mixed
+     */
     private static function get($endpoint = '/rssdd_magnet.php')
     {
         $cu = curl_init();
