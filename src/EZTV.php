63ed9001d9f7eed2f25fce4d8fb26a923aa903ac
pxgamer <owzie123@gmail.com>
2017-09-30 20:54:32 +0100

    Removed the get() method from EZTV and RARBG classes as it's now in the Parser trait

--- src/EZTV.php
+++ src/EZTV.php
@@ -23,29 +23,4 @@ class EZTV
     {
         return self::get('/ezrss.xml');
     }
-
-    /**
-     * Perform a GET request
-     *
-     * @param string $endpoint
-     * @return mixed
-     */
-    private static function get($endpoint = '/ezrss.xml')
-    {
-        $cu = curl_init();
-        curl_setopt_array(
-            $cu,
-            [
-                CURLOPT_URL            => self::BASE_URL . $endpoint,
-                CURLOPT_SSL_VERIFYPEER => 0,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_RETURNTRANSFER => 1,
-            ]
-        );
-        $response = curl_exec($cu);
-        $response = str_replace('&nbsp;', ' ', $response);
-        $xml = simplexml_load_string($response);
-
-        return self::xml2array($xml)['channel'][0]['item'];
-    }
 }
--- src/RARBG.php
+++ src/RARBG.php
@@ -23,30 +23,4 @@ class RARBG
     {
         return self::get('/rssdd_magnet.php');
     }
-
-    /**
-     * Perform a GET request
-     *
-     * @param string $endpoint
-     * @return mixed
-     */
-    private static function get($endpoint = '/rssdd_magnet.php')
-    {
-        $cu = curl_init();
-        curl_setopt_array(
-            $cu,
-            [
-                CURLOPT_URL => self::BASE_URL.$endpoint,
-                CURLOPT_SSL_VERIFYPEER => 0,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_USERAGENT => 'Torrent Parser PHP'
-            ]
-        );
-        $response = curl_exec($cu);
-        $response = str_replace('&nbsp;', ' ', $response);
-        $xml = simplexml_load_string($response);
-
-        return self::xml2array($xml)['channel'][0]['item'];
-    }
 }
