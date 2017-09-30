ef16f621f139607c2d6cbcc245b98b529ee5abd9
pxgamer <owzie123@gmail.com>
2017-09-30 21:15:30 +0100

    Added function to create a Collection of Torrent instances to EZTV class

--- src/EZTV.php
+++ src/EZTV.php
@@ -2,6 +2,7 @@
 
 namespace pxgamer\TorrentParser;
 
+use Illuminate\Support\Collection;
 use pxgamer\TorrentParser\Traits\Parser;
 
 /**
@@ -21,6 +22,35 @@ class EZTV
      */
     public static function latest()
     {
-        return self::get(self::BASE_URL . '/ezrss.xml');
+        $data = self::get(self::BASE_URL . '/ezrss.xml');
+
+        return self::createCollection($data);
+    }
+
+    /**
+     * Create a new Collection of Torrent instances
+     *
+     * @param array $responseData
+     * @return Collection
+     */
+    private static function createCollection($responseData)
+    {
+        $collection = new Collection();
+        foreach ($responseData as $element) {
+            $torrent = new Torrent();
+
+            $torrent->title = $element['title'] ?? null;
+            $torrent->link = $element['link'] ?? null;
+            $torrent->category = $element['category'] ?? null;
+            $torrent->date = $element['pubDate'] ?? null;
+
+            if ($torrent->date) {
+                $torrent->date = new \DateTime($torrent->date);
+            }
+
+            $collection[] = $torrent;
+        }
+
+        return $collection;
     }
 }
