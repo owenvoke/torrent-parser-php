04dc0627a8b2020ade5bfde21b53a26c150a4afb
pxgamer <owzie123@gmail.com>
2018-12-20 10:43:52 +0000

    Increase the speed of the Parser trait

--- src/Traits/Parser.php
+++ src/Traits/Parser.php
@@ -19,9 +19,8 @@ trait Parser
 
         if ($xml) {
             foreach ($xml->children() as $r) {
-                $t = [];
-                if (!is_null($r) && count($r->children()) == 0) {
-                    $arr[$r->getName()] = strval($r);
+                if ($r !== null && count($r->children()) === 0) {
+                    $arr[$r->getName()] = (string)$r;
                 } else {
                     $arr[$r->getName()][] = self::xml2array($r);
                 }
